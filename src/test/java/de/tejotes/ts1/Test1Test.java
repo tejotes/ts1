7e170d65cd3b0a276436c5f0338c03bd613aa30a
Thomas Steinhart <thomas.steinhart@tup.com>
2012-09-11 22:53:20 +0200

    removed @Test annotation

--- src/test/java/de/tejotes/ts1/Test1Test.java
+++ src/test/java/de/tejotes/ts1/Test1Test.java
@@ -1,10 +1,7 @@
 package de.tejotes.ts1;
 
-import org.junit.Test;
-
 public class Test1Test extends junit.framework.TestCase {
 
-    @Test
     public void testSayHello() throws Exception {
         Test1 t = new Test1();
         String hello = t.sayHello("Boss");
